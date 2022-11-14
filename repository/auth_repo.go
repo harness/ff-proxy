3c4dc93b9ceac2624454b18393cbb8b2c2b8ece5
Conor Murray <conor.murray@harness.io>
2022-11-14 13:52:33 +0000

    (FFM-5279) Prevent proxies deleting each others config (#102)
    * (FFM-5279) Prevent proxies deleting each others config
    
    * (FFM-5279) Make getAll private

--- repository/auth_repo.go
+++ repository/auth_repo.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/harness/ff-proxy/cache"
@@ -24,9 +25,9 @@ func NewAuthRepo(c cache.Cache, config map[domain.AuthAPIKey]string) (AuthRepo, 
 		return ar, nil
 	}
 
-	// cleanup old unused keys before we set the new ones
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-	ar.cache.RemoveAll(ctx, AuthKey)
+	// cleanup old unused keys for specified envs before we set the new ones
+	ar.clearCachedKeys(ctx, config)
 
 	if err := ar.Add(ctx, apiEnvMapToAuthConfig(config)...); err != nil {
 		cancel()
@@ -35,6 +36,35 @@ func NewAuthRepo(c cache.Cache, config map[domain.AuthAPIKey]string) (AuthRepo, 
 	cancel()
 
 	return ar, nil
+}
+
+// clearCachedKeys clears any old existing keys for the specified environments we want to configure
+// the reason we do this instead of RemoveAll is a user may have one proxy running against env a
+// and another proxy running against env b, both connected to the same redis cache
+// we only want to clear old keys related to the envs this individual proxy is configuring and not
+// wipe anyone else's data
+func (a AuthRepo) clearCachedKeys(ctx context.Context, newConfig map[domain.AuthAPIKey]string) {
+	// get all auth keys
+	currentConfig, ok := a.getAll(ctx)
+	if !ok {
+		// no keys exist so we can safely return
+		return
+	}
+
+	// what envs are being set in new config
+	envsToAdd := map[string]struct{}{}
+	for _, env := range newConfig {
+		envsToAdd[env] = struct{}{}
+	}
+
+	// remove all existing keys for affected envs
+	for key, env := range currentConfig {
+		// if env exists in envsToAdd map delete the key
+		_, ok := envsToAdd[env]
+		if ok {
+			a.cache.Remove(ctx, AuthKey, string(key))
+		}
+	}
 }
 
 // Add adds environment api key hash pairs to the cache
@@ -61,6 +91,24 @@ func (a AuthRepo) Get(ctx context.Context, key domain.AuthAPIKey) (string, bool)
 	}
 
 	return string(environment), true
+}
+
+// getAll gets all values from auth repo
+func (a AuthRepo) getAll(ctx context.Context) (map[domain.AuthAPIKey]string, bool) {
+
+	results, err := a.cache.GetAll(ctx, AuthKey)
+	if err != nil {
+		return map[domain.AuthAPIKey]string{}, false
+	}
+
+	keys := map[domain.AuthAPIKey]string{}
+	for key, b := range results {
+		var env = strings.Trim(string(b), "\"")
+
+		keys[domain.AuthAPIKey(key)] = env
+	}
+
+	return keys, true
 }
 
 func apiEnvMapToAuthConfig(config map[domain.AuthAPIKey]string) []domain.AuthConfig {
