795107d5bb6c14f66c549bf0ece5675e71720336
Conor Murray <conor.murray@harness.io>
2022-11-18 12:43:44 +0000

    (MAINT) Debug pushpin

--- start.sh
+++ start.sh
@@ -2,4 +2,12 @@
 { ./app/ff-proxy; } &
 { pushpin; } &
 wait -n
+echo "condure logs"
+cat /pushpin/log/condure.log
+echo "pushpin-handler logs"
+cat /pushpin/log/pushpin-handler.log
+echo "pushpin-proxy logs"
+cat /pushpin/log/pushpin-proxy.log
+echo "zurl logs"
+cat /pushpin/log/zurl.log
 pkill -P $$
