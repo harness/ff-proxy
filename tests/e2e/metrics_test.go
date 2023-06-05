779b6b765898ed6a1e37b1679e3337f241b29f3d
James Cox <james.cox@harness.io>
2023-06-05 12:52:04 +0100

    update again

--- tests/e2e/metrics_test.go
+++ tests/e2e/metrics_test.go
@@ -22,9 +22,9 @@ func TestMetrics(t *testing.T) {
 	// Note: This tests the proxy can successfully forward on metrics and target metrics to Saas as an integration test
 	// if SaaS is experiencing high load or a slowdown in processing metrics these tests may time out and fail
 	// in this case either rerun or manually verify everything is fine before skipping
-	//if !RunMetricsTests() {
-	t.Skip("skipping metrics tests")
-	//}
+	if !RunMetricsTests() {
+		t.Skip("skipping metrics tests")
+	}
 	targetIdentifier := "metricsTarget"
 
 	// auth against proxy
@@ -90,6 +90,7 @@ func TestMetrics(t *testing.T) {
 		flag, err := getFeatureFlag("bool-flag1", GetEnvironmentIdentifier())
 		if err != nil {
 			t.Errorf("failed to fetch flags: err %s", err)
+			continue
 		}
 
 		// check that flag metrics status and last access are correct
