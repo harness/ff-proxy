6b6f505dbb955e0b9e8fc5e9826537463034fd82
James Cox <james.cox@harness.io>
2023-10-16 10:38:51 +0100

    FFM-9522 Create two orgs with projects for e2e tests

--- go.mod
+++ go.mod
@@ -5,9 +5,11 @@ go 1.20
 require (
 	cloud.google.com/go/profiler v0.3.1
 	github.com/alicebob/miniredis/v2 v2.30.4
+	github.com/avast/retry-go v3.0.0+incompatible
 	github.com/deepmap/oapi-codegen v1.11.0
 	github.com/fanout/go-gripcontrol v0.0.0-20221004121322-47dacded330e
 	github.com/fanout/go-pubcontrol v0.0.0-20221004123744-4d052349ceb5
+	github.com/getkin/kin-openapi v0.98.0
 	github.com/go-kit/kit v0.12.0
 	github.com/go-redis/cache/v8 v8.4.4
 	github.com/go-redis/redis/v8 v8.11.4
@@ -15,6 +17,7 @@ require (
 	github.com/golang-jwt/jwt/v4 v4.4.3
 	github.com/google/uuid v1.3.0
 	github.com/harness/ff-golang-server-sdk v0.1.10
+	github.com/harness/ff-proxy v1.0.0
 	github.com/hashicorp/go-multierror v1.1.1
 	github.com/joho/godotenv v1.4.0
 	github.com/json-iterator/go v1.1.12
@@ -40,7 +43,6 @@ require (
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/getkin/kin-openapi v0.98.0 // indirect
 	github.com/go-openapi/jsonpointer v0.19.5 // indirect
 	github.com/go-openapi/swag v0.21.1 // indirect
 	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
--- tests/e2e/testhelpers/apikey.go
+++ tests/e2e/testhelpers/apikey.go
@@ -1,37 +1,44 @@
 package testhelpers
 
+import (
+	"context"
+	"strconv"
+
+	admin "github.com/harness/ff-proxy/v2/gen/admin"
+)
+
 // APIKeyName ...
-//const (
-//	APIKeyTypeServer = "Server"
-//	APIKeyTypeClient = "Client"
-//	APIKeyExpiredAt  = ""
-//)
-//
-//// AddAPIKey ...
-//func AddAPIKey(reqBody admin.AddAPIKeyJSONRequestBody, projectIdentifier string, envIdentifier string) (*admin.AddAPIKeyResponse, error) {
-//	client := DefaultClient()
-//
-//	segment, err := client.AddAPIKey(context.Background(), &admin.AddAPIKeyParams{
-//		AccountIdentifier:     GetDefaultAccount(),
-//		OrgIdentifier:         GetDefaultOrg(),
-//		EnvironmentIdentifier: envIdentifier,
-//		ProjectIdentifier:     projectIdentifier,
-//	}, reqBody, AddAuthToken)
-//
-//	if err != nil {
-//		return nil, err
-//	}
-//	return admin.ParseAddAPIKeyResponse(segment)
-//}
-//
-//// GetAddAPIKeyBody ...
-//func GetAddAPIKeyBody(identifier string, apiKeyType string, name string, description string, expiredAt string) admin.AddAPIKeyJSONRequestBody {
-//	expiredAtInt, _ := strconv.Atoi(expiredAt)
-//	return admin.AddAPIKeyJSONRequestBody{
-//		Description: &description,
-//		ExpiredAt:   &expiredAtInt,
-//		Identifier:  identifier,
-//		Name:        name,
-//		Type:        admin.ApiKeyRequestType(apiKeyType),
-//	}
-//}
+const (
+	APIKeyTypeServer = "Server"
+	APIKeyTypeClient = "Client"
+	APIKeyExpiredAt  = ""
+)
+
+// AddAPIKey ...
+func AddAPIKey(org string, reqBody admin.AddAPIKeyJSONRequestBody, projectIdentifier string, envIdentifier string) (*admin.AddAPIKeyResponse, error) {
+	client := DefaultClient()
+
+	segment, err := client.AddAPIKey(context.Background(), &admin.AddAPIKeyParams{
+		AccountIdentifier:     admin.AccountQueryParam(GetDefaultAccount()),
+		OrgIdentifier:         admin.OrgQueryParam(org),
+		EnvironmentIdentifier: admin.EnvironmentQueryParam(envIdentifier),
+		ProjectIdentifier:     admin.ProjectQueryParam(projectIdentifier),
+	}, reqBody, AddAuthToken)
+
+	if err != nil {
+		return nil, err
+	}
+	return admin.ParseAddAPIKeyResponse(segment)
+}
+
+// GetAddAPIKeyBody ...
+func GetAddAPIKeyBody(identifier string, apiKeyType string, name string, description string, expiredAt string) admin.AddAPIKeyJSONRequestBody {
+	expiredAtInt, _ := strconv.Atoi(expiredAt)
+	return admin.AddAPIKeyJSONRequestBody{
+		Description: &description,
+		ExpiredAt:   &expiredAtInt,
+		Identifier:  identifier,
+		Name:        name,
+		Type:        admin.ApiKeyRequestType(apiKeyType),
+	}
+}
