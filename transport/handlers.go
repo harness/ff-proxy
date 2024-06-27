58aee8382f906e93de55834f24f0892be514a4f0
James Cox <james.cox@harness.io>
2024-06-27 11:08:19 +0100

    use logging response writer for debugging

--- transport/handlers.go
+++ transport/handlers.go
@@ -2,7 +2,9 @@ package transport
 
 import (
 	"context"
+	"fmt"
 	"net/http"
+	"sync"
 
 	"github.com/go-kit/kit/endpoint"
 	"github.com/harness/ff-proxy/v2/log"
@@ -24,7 +26,9 @@ type encodeResponseFunc func(ctx context.Context, w http.ResponseWriter, resp in
 func NewUnaryHandler(e endpoint.Endpoint, dec decodeRequestFunc, enc encodeResponseFunc, errorEncoder errorEncoderFunc, l log.Logger) echo.HandlerFunc {
 	return func(c echo.Context) error {
 		ctx := c.Request().Context()
-		w := c.Response().Writer
+
+		// Use logging response writer to try and catch where superfluous write header calls are coming from
+		w := newLoggingResponseWriter(c.Request(), c.Response().Writer, l)
 
 		req, err := dec(c, l)
 		if err != nil {
@@ -41,4 +45,58 @@ func NewUnaryHandler(e endpoint.Endpoint, dec decodeRequestFunc, enc encodeRespo
 		}
 		return nil
 	}
+}
+
+type loggingResponseWriter struct {
+	*sync.Mutex
+	log log.Logger
+
+	writer http.ResponseWriter
+	req    *http.Request
+
+	writeCounts       int
+	writeHeaderCounts int
+}
+
+func newLoggingResponseWriter(r *http.Request, w http.ResponseWriter, l log.Logger) *loggingResponseWriter {
+	return &loggingResponseWriter{
+		Mutex:             &sync.Mutex{},
+		log:               l,
+		writer:            w,
+		req:               r,
+		writeCounts:       0,
+		writeHeaderCounts: 0,
+	}
+}
+
+func (l *loggingResponseWriter) Header() http.Header {
+	return l.writer.Header()
+}
+
+func (l *loggingResponseWriter) Write(bytes []byte) (int, error) {
+	l.Lock()
+	defer func() {
+		l.writeCounts += 1
+		l.Unlock()
+	}()
+
+	if l.writeCounts > 0 {
+		l.log.Error("more than one write header call", "url", l.req.URL.String(), "resp_body", fmt.Sprintf("%s", bytes), "write_counts", l.writeCounts)
+	}
+
+	return l.Write(bytes)
+}
+
+func (l *loggingResponseWriter) WriteHeader(statusCode int) {
+	l.Lock()
+	defer func() {
+		l.writeHeaderCounts += 1
+		l.Unlock()
+	}()
+
+	if l.writeHeaderCounts > 0 {
+		l.log.Error("more than one write header call", "url", l.req.URL.String(), "status_code", statusCode)
+	}
+
+	l.writer.WriteHeader(statusCode)
 }
