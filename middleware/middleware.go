44715323396ec654ddc1fd6b36035b5d3a53cea4
James Cox <james.cox@harness.io>
2025-05-27 10:27:28 +0100

    update deps

--- middleware/middleware.go
+++ middleware/middleware.go
@@ -8,7 +8,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/golang-jwt/jwt"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/google/uuid"
 	"github.com/harness/ff-proxy/domain"
 	"github.com/harness/ff-proxy/log"
