421458a40a80e42c36e8dcf6d1e09872c3529f73
James Cox <jcox250@protonmail.com>
2025-09-24 11:43:47 +0100

    unskip

--- tests/e2e/environment_refreshing_test.go
+++ tests/e2e/environment_refreshing_test.go
@@ -25,7 +25,6 @@ import (
 // Also tests that if we create an environment in a project with scope=selected that the
 // config won't be sent to the Proxy
 func TestEnvironmentCreation(t *testing.T) {
-	t.Skip()
 	var (
 		orgTwo     = GetSecondaryOrgIdentifier()
 		projectTwo = GetSecondaryProjectIdentifier() // Scope = all
@@ -225,7 +224,6 @@ func withRetry(conditionFn func(r *http.Response) bool, fn retryFn) (*http.Respo
 }
 
 func TestEnvironmentDeletion(t *testing.T) {
-	t.Skip()
 	var (
 		orgTwo        = GetSecondaryOrgIdentifier()
 		projectTwo    = GetSecondaryProjectIdentifier() // Scope = all
